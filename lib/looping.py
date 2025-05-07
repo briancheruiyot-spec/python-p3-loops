a430ffba340d0844915112372c88088d34fd37ef
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-07 20:21:18 +0300

    Updated files

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    # Countdown from 10 to 1 and then print "Happy New Year!"
+    number = 10
+    while number > 0:
+        print(number)
+        number -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # Return a list of squared integers from the input list
+    return [x**2 for x in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # Print numbers from 1 to 100 with conditions for Fizz, Buzz, and FizzBuzz
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
